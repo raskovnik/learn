2e6d383e9b253b91e64114758b0a57d2a3fa1d9c
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2021-08-30 20:37:08 +0300

    oop

--- rust/oop/blog/src/lib.rs
+++ rust/oop/blog/src/lib.rs
@@ -10,11 +10,39 @@ impl Post {
             content: String::new(),
         }
     }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
 }
 
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendindReview {})
+    }
+}
 
+
+struct PendindReview{}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
