fef1d2ea561689609ea10e6b7aee785ad7bd4458
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-08-06 17:00:23 +0300

    practice

---
+++ Dart/exercises/low_high.dart
@@ -0,0 +1,24 @@
+import "dart:io";
+
+void main() {
+  int max = 100, min = 0, attempts = 0;
+
+  while (true) {
+    attempts += 1;
+    int guess = (max + min) ~/ 2;
+
+    print("My guess is the number is: $guess");
+    stdout.write("Is the guess high, low or correct?: ");
+    String correct = stdin.readLineSync()!;
+
+    if (correct.toLowerCase() == "high") {
+      max = guess;
+    } else if (correct.toLowerCase() == "low") {
+      min = guess;
+    } else if (correct.toLowerCase() == "correct" || correct.toLowerCase().contains("cor")) {
+      print("The number was $guess and gotten after $attempts attempts");
+      break;
+    }
+
+  }
+}
---
+++ Dart/exercises/max_of_three.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+
+void main() {
+  stdout.write("Enter three numbers: ");
+  List<String> input = stdin.readLineSync()!.split(" ");
+  var mmax;
+  // a > b ? a > c ? mmax = a: mmax = c : b > c ? mmax = b: mmax = c;
+  int.parse(input[0]) > int.parse(input[1])? int.parse(input[0]) > int.parse(input[2])? mmax = input[0] : mmax = input[2]: int.parse(input[1]) > int.parse(input[2]) ? mmax = input[1]: mmax = input[2];
+  print("The largest is $mmax");
+}
