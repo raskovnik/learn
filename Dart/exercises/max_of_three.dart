ff208c3664e6d94054a7eab20428c053060f50e1
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-08-07 19:39:39 +0300

    practice

--- Dart/exercises/max_of_three.dart
+++ Dart/exercises/max_of_three.dart
@@ -5,6 +5,6 @@ void main() {
   List<String> input = stdin.readLineSync()!.split(" ");
   var mmax;
   // a > b ? a > c ? mmax = a: mmax = c : b > c ? mmax = b: mmax = c;
-  int.parse(input[0]) > int.parse(input[1])? int.parse(input[0]) > int.parse(input[2])? mmax = input[0] : mmax = input[2]: int.parse(input[1]) > int.parse(input[2]) ? mmax = input[1]: mmax = input[2];
+  int.parse(input[0])>int.parse(input[1])? int.parse(input[0]) > int.parse(input[2])? mmax = input[0]: mmax = input[2]: int.parse(input[1]) > int.parse(input[2]) ? mmax = input[1]: mmax = input[2];
   print("The largest is $mmax");
 }
