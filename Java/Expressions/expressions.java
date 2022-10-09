09c33a9106594d9f3e8934cc7e62c31dd7b11165
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-10-09 14:54:46 +0300

    Expressions

---
+++ Java/Expressions/expressions.java
@@ -0,0 +1,13 @@
+public class expressions {
+    public static void main(String[] args) {
+        double friends= 10; // what a lie
+        friends = friends + 1;
+        System.out.println(friends);
+
+        friends = friends % 10;
+        System.out.println(friends);
+
+        friends = (double) friends / 2;
+        System.out.println(friends);
+    }
+}
---
+++ Java/basic_gui/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter Your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are "+height+"cm tall");
+    }
+}
