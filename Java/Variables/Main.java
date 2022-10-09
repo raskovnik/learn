9692d9a38d02d7942e69517b4929be146fdd51a7
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-10-09 14:38:12 +0300

    Variables

---
+++ Java/Variables/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 123;
+        float y = 3.14f;
+        boolean z = false;
+        char symbol = '@';
+        String name = "Hello there";
+
+
+        System.out.println("My number is: "+x);
+        System.out.println(y);
+        System.out.println("Hello " + name);
+    }
+}
---
+++ Java/Variables/swap.java
@@ -0,0 +1,15 @@
+public class swap {
+    public static void main(String[] args) {
+        String x = "water";
+        String y = "Kool-aid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+
+    }
+}
