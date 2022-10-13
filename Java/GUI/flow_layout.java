6d2a296351aeff06d241f9ee95ff3c7945151957
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-10-13 23:52:01 +0300

    GUI

---
+++ Java/GUI/flow_layout.java
@@ -0,0 +1,25 @@
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class flow_layout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.setSize(500,500);
+        
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+
+        frame.setVisible(true);
+        
+    }
+}
