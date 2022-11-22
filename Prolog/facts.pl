a4478a5a24131c3359dbfb2840440ec0ce6f5ff0
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-11-22 21:56:05 +0300

    recursion

--- Prolog/facts.pl
+++ Prolog/facts.pl
@@ -25,5 +25,5 @@ mother_of(X, Y) :- female(X), parent(X, Y).
 g_father(X, Y) :- male(X), parent(X, Z), parent(Z, Y).
 g_mother(X, Y) :- female(X), parent(X, Z), parent(Z, Y).
 
-sister_of(X, Y) :- female(X), parent(Z, X), parent(Z, Y), male(Y); female(X), female(Y), parent(Z, X), parent(Z, Y).
-brother_of(X, Y) :- male(X), parent(Z, X), parent(Z, Y), male(Y); male(X), female(Y), parent(Z, X), parent(Z, Y).
+sister_of(X, Y) :- female(X), parent(Z, X), parent(Z, Y), male(Y); female(X), female(Y), X\==Y, parent(Z, X), parent(Z, Y).
+brother_of(X, Y) :- male(X), parent(Z, X), parent(Z, Y), male(Y), X\==Y; male(X), female(Y), parent(Z, X), parent(Z, Y).
