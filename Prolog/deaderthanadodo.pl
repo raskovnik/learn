3845e11c34b0093673c703d1ca514bff29a7a859
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2022-11-22 21:07:31 +0300

    Facts and rules

--- Prolog/deaderthanadodo.pl
+++ Prolog/deaderthanadodo.pl
@@ -1,3 +1,4 @@
 woman(mia).
 likes(john, mia).
 is_boyfriend(john, mia) :- likes(john, mia), woman(mia).
+parent(james, jimmy).
--- Prolog/facts.pl
+++ Prolog/facts.pl
@@ -1,6 +1,6 @@
 male(jack).
 male(oliver).
-male(all).
+# male(all).
 male(james).
 male(shaw).
 male(harry).
@@ -10,12 +10,13 @@ female(lily).
 female(jess).
 parent(jack, jess).
 parent(jack, lily).
+parent(jack, james).
 parent(hellen, jess).
 parent(hellen, lily).
 parent(oliver, james).
-parent(sophie, james).
+# parent(sophie, james).
 parent(jess, simon).
-parent(all, simon).
+# parent(all, simon).
 parent(lily, harry).
 parent(jess, harry).
 
@@ -24,9 +25,5 @@ mother_of(X, Y) :- female(X), parent(X, Y).
 g_father(X, Y) :- male(X), parent(X, Z), parent(Z, Y).
 g_mother(X, Y) :- female(X), parent(X, Z), parent(Z, Y).
 
-# sister
-# brother
-# aunt
-# uncle
-# husband
-# wife
+sister_of(X, Y) :- female(X), parent(Z, X), parent(Z, Y), male(Y); female(X), female(Y), parent(Z, X), parent(Z, Y).
+brother_of(X, Y) :- male(X), parent(Z, X), parent(Z, Y), male(Y); male(X), female(Y), parent(Z, X), parent(Z, Y).
